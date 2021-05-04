6b8a4f796544bdad43aed8f8063e03d64aff0bb9
0shade0 <damcupi@student.math.hr>
2021-05-04 15:15:46 +0200

    Godišnja doba uvedena.

--- Kriptografija/src/main/java/MainClass.java
+++ Kriptografija/src/main/java/MainClass.java
@@ -1,6 +1,11 @@
+import godisnjadoba.GodisnjaDoba;
+
 public class MainClass {
 
   public static void main(String[] args) {
     System.out.println("Hello World!");
+
+    var zima = new GodisnjaDoba("Zima");
+    System.out.println(zima);
   }
 }
